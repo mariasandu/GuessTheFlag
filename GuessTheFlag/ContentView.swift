daa34dee932b648daccd6895c3e2778b0f5e063d
Maria Sandu <Maria.Sandu@emids.com>
2023-09-29 09:19:00 +0300

    Fix accessibility isuue

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -19,6 +19,21 @@ struct ContentView: View {
     @State private var shouldReset = false
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
@@ -46,6 +61,7 @@ struct ContentView: View {
                                 .renderingMode(.original)
                                 .cornerRadius(30)
                                 .shadow(radius: 50)
+                                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         }
                     }
                 }
@@ -55,9 +71,6 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 Text("Score: \(score)")
                     .titleStyle()
-//                    .modifier(Title())
-//                    .foregroundColor(.white)
-//                    .font(.title.bold())
             }
             
         }
@@ -73,7 +86,7 @@ struct ContentView: View {
     
     func flagTapped(_ number: Int) {
         noOfQuestions += 1
-        if noOfQuestions > 3 {
+        if noOfQuestions > 7 {
             shouldReset = true
         }
         
@@ -114,8 +127,6 @@ extension View {
         modifier(TitleModifier())
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
